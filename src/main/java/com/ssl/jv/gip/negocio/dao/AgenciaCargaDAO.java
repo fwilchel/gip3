07f4d2a0b21717e47dc37005dfae3d6a7801c40e
juancho1280 <juan.c.bernal.rincon@gmail.com>
2014-12-14 10:15:15 -0500

    Maestro de Agencias De Carga

--- src/main/java/com/ssl/jv/gip/negocio/dao/AgenciaCargaDAO.java
+++ src/main/java/com/ssl/jv/gip/negocio/dao/AgenciaCargaDAO.java
@@ -6,24 +6,45 @@ import java.util.List;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
+import org.apache.log4j.Logger;
+
 import com.ssl.jv.gip.jpa.pojo.AgenciaCarga;
-import com.ssl.jv.gip.jpa.pojo.Ubicacion;
+
 
 @Stateless
 @LocalBean
-public class AgenciaCargaDAO extends GenericDAO<Ubicacion> implements AgenciaCargaDAOLocal {
+public class AgenciaCargaDAO extends GenericDAO<AgenciaCarga> implements AgenciaCargaDAOLocal {
 
+	private static final Logger LOGGER = Logger.getLogger(AgenciaCargaDAO.class);
+
+	public AgenciaCargaDAO(){
+		this.persistentClass = AgenciaCarga.class;
+	}
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<AgenciaCarga> consultarAgenciaCargaPorFiltro(
 			AgenciaCarga pFiltro) {
-List<AgenciaCarga> listado = new ArrayList<AgenciaCarga>();
-		
+		List<AgenciaCarga> listado = new ArrayList<AgenciaCarga>();
+		String query = null;
+
 		try{
-			listado = em.createQuery("select AgenciaCarga a from AgenciaCarga WHERE activo = true AND nombre like :nombre order by nombre").
+
+			if(pFiltro.getActivo()){
+				query = "select a from AgenciaCarga a WHERE a.activo = true AND a.nombre like :nombre order by a.nombre";
+			}else{
+				query= "select a from AgenciaCarga a WHERE a.nombre like :nombre order by a.nombre";
+			}
+			
+			listado = em.createQuery(query).
 					setParameter("nombre", PORCENTAJE_LIKE + pFiltro.getNombre() == null ? "" : pFiltro.getNombre() + PORCENTAJE_LIKE).
 					getResultList();
+
+
+
 		} catch(Exception e){
-			
+			LOGGER.error(e + "********Error consultando agencias de carga");
+			return null;
 		}
 		return listado;
 	}
--- src/main/java/com/ssl/jv/gip/negocio/ejb/MaestrosEJBLocal.java
+++ src/main/java/com/ssl/jv/gip/negocio/ejb/MaestrosEJBLocal.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 import javax.ejb.Local;
 
+import com.ssl.jv.gip.jpa.pojo.AgenciaCarga;
 import com.ssl.jv.gip.jpa.pojo.Ubicacion;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface MaestrosEJBLocal.
+ */
 @Local
 public interface MaestrosEJBLocal {
 	
@@ -47,5 +52,36 @@ public interface MaestrosEJBLocal {
 	 * @return the ubicacion
 	 */
 	public Ubicacion actualizarUbicacion(Ubicacion pEntidad);
+	
+	/**
+	 * Consultar agencias carga.
+	 *
+	 * @return the list
+	 */
+	public List<AgenciaCarga> consultarAgenciasCarga();
+	
+	/**
+	 * Consultar agencias carga.
+	 *
+	 * @param pFiltro the filtro
+	 * @return the list
+	 */
+	public List<AgenciaCarga> consultarAgenciasCarga(AgenciaCarga pFiltro);
+	
+	/**
+	 * Crear agencia carga.
+	 *
+	 * @param pEntidad the entidad
+	 * @return the agencia carga
+	 */
+	public AgenciaCarga crearAgenciaCarga(AgenciaCarga pEntidad);
+	
+	/**
+	 * Actualizar agencia carga.
+	 *
+	 * @param pEntidad the entidad
+	 * @return the agencia carga
+	 */
+	public AgenciaCarga actualizarAgenciaCarga(AgenciaCarga pEntidad);
 	
 }
