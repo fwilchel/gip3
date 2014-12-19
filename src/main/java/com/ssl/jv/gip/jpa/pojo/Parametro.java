5389712a35a768d7a7348f0cc34377dd3b09c69d
fwilchel <fwilchel@hotmail.com>
2014-12-18 19:22:52 -0500

    Nuevo pojo para Parametros generales del sistema

---
+++ src/main/java/com/ssl/jv/gip/jpa/pojo/Parametro.java
@@ -0,0 +1,44 @@
+package com.ssl.jv.gip.jpa.pojo;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+/**
+ * The persistent class for the paises database table.
+ * 
+ */
+@Entity
+@Table(name="parametro")
+public class Parametro implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Integer id;
+	private String nombre;
+	private String valor;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getValor() {
+		return valor;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+
+}
