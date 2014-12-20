972c2733a9c628ac26519447d5236911fe33d908
Nicolas <Nicolas@NICOROM>
2014-12-20 18:37:50 -0500

    documento dato contribucion cafetera

--- src/main/java/com/ssl/jv/gip/negocio/ejb/ComercioExterior.java
+++ src/main/java/com/ssl/jv/gip/negocio/ejb/ComercioExterior.java
@@ -1,7 +1,14 @@
 package com.ssl.jv.gip.negocio.ejb;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+
+import com.ssl.jv.gip.negocio.dao.DocumentoDAO;
+import com.ssl.jv.gip.negocio.dto.DatoContribucionCafeteraDTO;
 
 /**
  * Session Bean implementation class ComercioExterior
@@ -10,11 +17,18 @@ import javax.ejb.Stateless;
 @LocalBean
 public class ComercioExterior implements ComercioExteriorLocal {
 
+	@EJB
+	private DocumentoDAO documentoDAO;
+	
     /**
      * Default constructor. 
      */
     public ComercioExterior() {
         // TODO Auto-generated constructor stub
     }
+    
+	public List<DatoContribucionCafeteraDTO> consultarDatosContrubicionCafetera(Map<String, Object> parametros){
+		return documentoDAO.consultarDatosContrubicionCafetera(parametros);
+	}
 
 }
