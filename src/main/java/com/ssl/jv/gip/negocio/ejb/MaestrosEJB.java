7046f8d9b5c7b05488db646be6f83375e5b2fb39
juancho1280 <juan.c.bernal.rincon@gmail.com>
2014-12-26 20:23:23 -0500

    Corrigiendo GIT!!!!!

--- src/main/java/com/ssl/jv/gip/negocio/ejb/MaestrosEJB.java
+++ src/main/java/com/ssl/jv/gip/negocio/ejb/MaestrosEJB.java
@@ -3,23 +3,21 @@ package com.ssl.jv.gip.negocio.ejb;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.log4j.Logger;
-
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
+import org.apache.log4j.Logger;
+
 import com.ssl.jv.gip.jpa.pojo.AgenciaCarga;
 import com.ssl.jv.gip.jpa.pojo.LugarIncoterm;
-import com.ssl.jv.gip.jpa.pojo.TerminoIncoterm;
 import com.ssl.jv.gip.jpa.pojo.ProductosXClienteComExtFiltroVO;
 import com.ssl.jv.gip.jpa.pojo.ProductosXClienteComext;
 import com.ssl.jv.gip.jpa.pojo.Ubicacion;
 import com.ssl.jv.gip.negocio.dao.AgenciaCargaDAO;
-import com.ssl.jv.gip.negocio.dao.IncotermXMedioTransDAO;
 import com.ssl.jv.gip.negocio.dao.LugarIncotermDAO;
+import com.ssl.jv.gip.negocio.dao.ProductoClienteComercioExteriorDAO;
 import com.ssl.jv.gip.negocio.dao.UbicacionDAO;
-import com.ssl.jv.gip.negocio.dao.UsuarioDAO;
 
 /**
  * Session Bean implementation class MaestrosEJB
@@ -27,228 +25,278 @@ import com.ssl.jv.gip.negocio.dao.UsuarioDAO;
 @Stateless
 @LocalBean
 public class MaestrosEJB implements MaestrosEJBLocal {
-	
+
 	private static final Logger LOGGER = Logger.getLogger(MaestrosEJB.class);
 
 	@EJB
 	private UbicacionDAO ubicacionDAO;
-	
+
 	@EJB
 	private AgenciaCargaDAO agenciaCargaDAO;
-	
+
 	@EJB
 	private LugarIncotermDAO lugarIncotermDAO;
-	
-	@EJB
-	private IncotermXMedioTransDAO incotermXMedioTransDAO;
-	
-    /**
-     * Default constructor. 
-     */
-    public MaestrosEJB() {
-        
-    }
-    
-    /* (non-Javadoc)
-     * @see com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#consultarUbicaciones()
-     */
-    @Override
-    public List<Ubicacion> consultarUbicaciones(){
-    	
-    	List<Ubicacion> listado = new ArrayList<Ubicacion>();
-    	
-    	try{
-    		listado = (List<Ubicacion>)ubicacionDAO.findAll();
-    	} catch(Exception e){
-    		
-    	}
-    	
-    	return listado;
-    	
-    }
-    
-    /* (non-Javadoc)
-     * @see com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#consultarUbicaciones(com.ssl.jv.gip.jpa.pojo.Ubicacion)
-     */
-    @Override
-    public List<Ubicacion> consultarUbicaciones(Ubicacion pFiltro){
-    	
-    	List<Ubicacion> listado = new ArrayList<Ubicacion>();
-    	
-    	try{
-    		listado = ubicacionDAO.consultarUbicacionPorFiltro(pFiltro);
-    	} catch(Exception e){
-    		
-    	}
-    	
-    	return listado;
-    	
-    }
-    
-    /* (non-Javadoc)
-     * @see com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#consultarUbicacion(java.lang.Long)
-     */
-    @Override
-    public Ubicacion consultarUbicacion(Long pId){
-    	
-    	Ubicacion entidad = new Ubicacion();
-    	
-    	try{
-    		entidad = ubicacionDAO.findByPK(pId);
-    	} catch(Exception e){
-    		
-    	}
-    	
-    	return entidad;
-    	
-    }
-    
-    /* (non-Javadoc)
-     * @see com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#crearUbicacion(com.ssl.jv.gip.jpa.pojo.Ubicacion)
-     */
-    @Override
-    public Ubicacion crearUbicacion(Ubicacion pEntidad){
-    	    	
-    	try{
-    		pEntidad = ubicacionDAO.add(pEntidad);
-    	} catch(Exception e){
-    		
-    	}
-    	
-    	return pEntidad;
-    	
-    }
-    
-    /* (non-Javadoc)
-     * @see com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#actualizarUbicacion(com.ssl.jv.gip.jpa.pojo.Ubicacion)
-     */
-    @Override
-    public Ubicacion actualizarUbicacion(Ubicacion pEntidad){
-    	    	
-    	try{
-    		ubicacionDAO.update(pEntidad);
-    	} catch(Exception e){
-    		
-    	}
-    	
-    	return pEntidad;
-    	
-    }
 
-	/* (non-Javadoc)
+	@EJB
+	private ProductoClienteComercioExteriorDAO productoClienteComercioExteriorDAO;
+
+	/**
+	 * Default constructor.
+	 */
+	public MaestrosEJB() {
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#consultarUbicaciones()
+	 */
+	@Override
+	public List<Ubicacion> consultarUbicaciones() {
+
+		List<Ubicacion> listado = new ArrayList<Ubicacion>();
+
+		try {
+			listado = (List<Ubicacion>) ubicacionDAO.findAll();
+		} catch (Exception e) {
+
+		}
+
+		return listado;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#consultarUbicaciones(com.
+	 * ssl.jv.gip.jpa.pojo.Ubicacion)
+	 */
+	@Override
+	public List<Ubicacion> consultarUbicaciones(Ubicacion pFiltro) {
+
+		List<Ubicacion> listado = new ArrayList<Ubicacion>();
+
+		try {
+			listado = ubicacionDAO.consultarUbicacionPorFiltro(pFiltro);
+		} catch (Exception e) {
+
+		}
+
+		return listado;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#consultarUbicacion(java.lang
+	 * .Long)
+	 */
+	@Override
+	public Ubicacion consultarUbicacion(Long pId) {
+
+		Ubicacion entidad = new Ubicacion();
+
+		try {
+			entidad = ubicacionDAO.findByPK(pId);
+		} catch (Exception e) {
+
+		}
+
+		return entidad;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#crearUbicacion(com.ssl.jv
+	 * .gip.jpa.pojo.Ubicacion)
+	 */
+	@Override
+	public Ubicacion crearUbicacion(Ubicacion pEntidad) {
+
+		try {
+			pEntidad = ubicacionDAO.add(pEntidad);
+		} catch (Exception e) {
+
+		}
+
+		return pEntidad;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#actualizarUbicacion(com.ssl
+	 * .jv.gip.jpa.pojo.Ubicacion)
+	 */
+	@Override
+	public Ubicacion actualizarUbicacion(Ubicacion pEntidad) {
+
+		try {
+			ubicacionDAO.update(pEntidad);
+		} catch (Exception e) {
+
+		}
+
+		return pEntidad;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#consultarAgenciasCarga()
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<AgenciaCarga> consultarAgenciasCarga() {
-		try{
-			return (List<AgenciaCarga>)agenciaCargaDAO.findAll();
-		}catch(Exception e){
-    		LOGGER.error(e + "Error consultando agencias de carga");
+		try {
+			return (List<AgenciaCarga>) agenciaCargaDAO.findAll();
+		} catch (Exception e) {
+			LOGGER.error(e + "Error consultando agencias de carga");
 			return null;
-    	}
+		}
 	}
 
-	/* (non-Javadoc)
-	 * @see com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#consultarAgenciasCarga(com.ssl.jv.gip.jpa.pojo.AgenciaCarga)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#consultarAgenciasCarga(com
+	 * .ssl.jv.gip.jpa.pojo.AgenciaCarga)
 	 */
 	@Override
 	public List<AgenciaCarga> consultarAgenciasCarga(AgenciaCarga pFiltro) {
-		try{
-			return (List<AgenciaCarga>)agenciaCargaDAO.consultarAgenciaCargaPorFiltro(pFiltro);
-		}catch(Exception e){
-    		LOGGER.error(e + "Error consultando agencias de carga por filtro");
+		try {
+			return (List<AgenciaCarga>) agenciaCargaDAO
+					.consultarAgenciaCargaPorFiltro(pFiltro);
+		} catch (Exception e) {
+			LOGGER.error(e + "Error consultando agencias de carga por filtro");
 			return null;
-    	}
+		}
 	}
 
 	@Override
 	public AgenciaCarga crearAgenciaCarga(AgenciaCarga pEntidad) {
-		try{
+		try {
 			return agenciaCargaDAO.add(pEntidad);
-		}catch(Exception e){
-    		LOGGER.error(e + "Error creando agencia de carga");
+		} catch (Exception e) {
+			LOGGER.error(e + "Error creando agencia de carga");
 			return null;
-    	}
+		}
 	}
 
 	@Override
 	public AgenciaCarga actualizarAgenciaCarga(AgenciaCarga pEntidad) {
-		try{
+		try {
 			agenciaCargaDAO.update(pEntidad);
 			return pEntidad;
-		}catch(Exception e){
-    		LOGGER.error(e + "Error actualizando agencia de carga");
+		} catch (Exception e) {
+			LOGGER.error(e + "Error actualizando agencia de carga");
 			return null;
-    	}
+		}
 	}
-	
-    /* (non-Javadoc)
-     * @see com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#consultarLugarIncoterm()
-     */
-    @Override
-    public List<LugarIncoterm> consultarLugarIncoterm(){
-    	
-    	List<LugarIncoterm> listado = new ArrayList<LugarIncoterm>();
-    	
-    	try{
-    		listado = (List<LugarIncoterm>)lugarIncotermDAO.findAll();
-    	} catch(Exception e){
-    		
-    	}
-    	
-    	return listado;
-    	
-    }
-    
-    /* (non-Javadoc)
-     * @see com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#consultarLugarIncoterm(java.lang.Long)
-     */
-    @Override
-    public LugarIncoterm consultarLugarIncoterm(Long pId){
-    	
-    	LugarIncoterm entidad = new LugarIncoterm();
-    	
-    	try{
-    		entidad = lugarIncotermDAO.findByPK(pId);
-    	} catch(Exception e){
-    		
-    	}
-    	
-    	return entidad;
-    	
-    }
 
-    /* (non-Javadoc)
-     * @see com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#crearLugarIncoterm(com.ssl.jv.gip.jpa.pojo.LugarIncoterm)
-     */
-    @Override
-    public LugarIncoterm crearLugarIncoterm(LugarIncoterm pEntidad){
-    	    	
-    	try{
-    		pEntidad = lugarIncotermDAO.add(pEntidad);
-    	} catch(Exception e){
-    		
-    	}
-    	
-    	return pEntidad;
-    	
-    }
-    
-    /* (non-Javadoc)
-     * @see com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#actualizarLugarIncoterm(com.ssl.jv.gip.jpa.pojo.LugarIncoterm)
-     */
-    @Override
-    public LugarIncoterm actualizarLugarIncoterm(LugarIncoterm pEntidad){
-    	    	
-    	try{
-    		lugarIncotermDAO.update(pEntidad);
-    	} catch(Exception e){
-    		
-    	}
-    	
-    	return pEntidad;
-    	
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#consultarLugarIncoterm()
+	 */
+	@Override
+	public List<LugarIncoterm> consultarLugarIncoterm() {
+
+		List<LugarIncoterm> listado = new ArrayList<LugarIncoterm>();
+
+		try {
+			listado = (List<LugarIncoterm>) lugarIncotermDAO.findAll();
+		} catch (Exception e) {
+
+		}
+
+		return listado;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#consultarLugarIncoterm(java
+	 * .lang.Long)
+	 */
+	@Override
+	public LugarIncoterm consultarLugarIncoterm(Long pId) {
+
+		LugarIncoterm entidad = new LugarIncoterm();
+
+		try {
+			entidad = lugarIncotermDAO.findByPK(pId);
+		} catch (Exception e) {
+
+		}
+
+		return entidad;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#crearLugarIncoterm(com.ssl
+	 * .jv.gip.jpa.pojo.LugarIncoterm)
+	 */
+	@Override
+	public LugarIncoterm crearLugarIncoterm(LugarIncoterm pEntidad) {
+
+		try {
+			pEntidad = lugarIncotermDAO.add(pEntidad);
+		} catch (Exception e) {
+
+		}
+
+		return pEntidad;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.ssl.jv.gip.negocio.ejb.MaestrosEJBLocal#actualizarLugarIncoterm(com
+	 * .ssl.jv.gip.jpa.pojo.LugarIncoterm)
+	 */
+	@Override
+	public LugarIncoterm actualizarLugarIncoterm(LugarIncoterm pEntidad) {
+
+		try {
+			lugarIncotermDAO.update(pEntidad);
+		} catch (Exception e) {
+
+		}
+
+		return pEntidad;
+
+	}
+
+	@Override
+	public List<ProductosXClienteComext> consultarProductosClienteComercioExteriorPorFiltro(
+			ProductosXClienteComExtFiltroVO filtroVO) {
+		return productoClienteComercioExteriorDAO.consultarPorFiltro(filtroVO);
+	}
+
+	@Override
+	public List<ProductosXClienteComext> consultarProductosClienteComercioExterior() {
+		return productoClienteComercioExteriorDAO.consultarTodos();
+	}
 
 }
