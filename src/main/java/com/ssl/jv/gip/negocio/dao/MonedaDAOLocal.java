291bd5c20bf5cc84bc53a25ee2997d9a6bffc2b8
Juan Buzon <j2buzon@yahoo.com>
2015-01-03 23:18:18 -0500

    Adición/Edición Cliente Producto Com Exterior
    Signed-off-by: Juan Buzon <j2buzon@yahoo.com>

---
+++ src/main/java/com/ssl/jv/gip/negocio/dao/MonedaDAOLocal.java
@@ -0,0 +1,14 @@
+package com.ssl.jv.gip.negocio.dao;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import com.ssl.jv.gip.jpa.pojo.Moneda;
+
+@Local
+public interface MonedaDAOLocal {
+
+	public List<Moneda> consultarTodas();
+
+}
