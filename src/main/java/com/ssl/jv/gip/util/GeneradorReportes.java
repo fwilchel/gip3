2e59d3aa0859a4bbb69e26159bf984b615be6bb7
fwilchel <fwilchel@hotmail.com>
2015-01-05 08:46:33 -0500

    Reportes PDF

--- src/main/java/com/ssl/jv/gip/util/GeneradorReportes.java
+++ src/main/java/com/ssl/jv/gip/util/GeneradorReportes.java
@@ -103,7 +103,7 @@ public class GeneradorReportes {
 			}else if (tipo.equals("xls")){
 				JRXlsExporter exporter = new JRXlsExporter();
 				exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-				exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOuputStream()));
+				exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream()));
 				SimpleXlsReportConfiguration configuration = new SimpleXlsReportConfiguration();
 				configuration.setOnePagePerSheet(true);
 				exporter.setConfiguration(configuration);
