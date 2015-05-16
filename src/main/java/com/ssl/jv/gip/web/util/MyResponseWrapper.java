513038d2769fc91839341c70d935e1c70471465e
Fredy <fwilchel@hotmail.com>
2015-05-16 11:53:50 -0500

    Ocultar status al descargar reporte Chrome

---
+++ src/main/java/com/ssl/jv/gip/web/util/MyResponseWrapper.java
@@ -0,0 +1,42 @@
+package com.ssl.jv.gip.web.util;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+import org.primefaces.util.Constants;
+
+public class MyResponseWrapper extends HttpServletResponseWrapper {
+
+	private final HttpServletRequest request;
+
+	public MyResponseWrapper(HttpServletRequest request, HttpServletResponse response) {
+		super(response);
+		this.request = request;
+		
+	}
+
+	@Override
+	public void addCookie(Cookie cookie) {
+		if(Constants.DOWNLOAD_COOKIE.equals(cookie.getName())){
+			String refererHeader = request.getHeader("Referer");
+			
+			
+			String contextPath = request.getContextPath();			
+			String requestURI = request.getRequestURI();
+			
+			String refererURI = refererHeader.substring(refererHeader.indexOf(contextPath));
+			if(!requestURI.equals(refererURI)){
+				String refererPath = refererURI.substring(0, refererURI.lastIndexOf("/"));
+				
+				cookie.setPath(refererPath);
+				
+				
+			}
+			
+		}
+		
+		super.addCookie(cookie);
+	}
+}
--- src/main/java/com/ssl/jv/gip/web/util/SecurityFilter.java
+++ src/main/java/com/ssl/jv/gip/web/util/SecurityFilter.java
@@ -20,64 +20,74 @@ import com.ssl.jv.gip.web.mb.MenuMB;
 
 public class SecurityFilter implements Filter {
 
-  private FilterConfig filterConfig = null;
+	private FilterConfig filterConfig = null;
 
-  public static final Logger LOGGER = Logger.getLogger(SecurityFilter.class);
+	public static final Logger LOGGER = Logger.getLogger(SecurityFilter.class);
 
-  public void destroy() {
-    this.filterConfig = null;
-  }
+	public void destroy() {
+		this.filterConfig = null;
+	}
 
-  @Override
-  public void doFilter(ServletRequest request, ServletResponse response,
-      FilterChain chain) throws IOException, ServletException {
-    HttpServletRequest req = (HttpServletRequest) request;
-    HttpServletResponse res = (HttpServletResponse) response;
-    try {
-      AplicacionMB appMB = (AplicacionMB) this.filterConfig.getServletContext().getAttribute("aplicacionMB");
-      String url = req.getRequestURI();
-      if (url.indexOf("javax.faces.resource") != -1 || url.endsWith("login.jsf") || url.endsWith("recordarContrasena.jsf")
-          || url.endsWith("cambiarContrasena.jsf") || (appMB != null && appMB.getDebug()) || url.endsWith("generadorReportes")
-          || url.endsWith("listaReportes.jsp") || url.endsWith("compilarReporte.jsp")) {
-        chain.doFilter(request, response);
-      } else {
-        LOGGER.debug(url);
-        HttpSession session = req.getSession(false);
-        if (session == null || session.getAttribute("menuMB") == null || ((MenuMB) session.getAttribute("menuMB")).getOpciones() == null) {
-          if (session != null) {
-            session.removeAttribute("menuMB");
-            session.invalidate();
-          }
-          session = req.getSession(true);
-          request.setAttribute(
-              "exception",
-              new Exception(
-                  AplicacionMB.getMessage("sesionCaducado", AplicacionMB.SPANISH)));
-          this.filterConfig
-              .getServletContext()
-              .getRequestDispatcher(
-                  "/ui/login.jsf;jsessionid="
-                  + session.getId())
-              .forward(request, response);
-        } else {
-          //TODO : Validar permiso a la opciÃ³n de menÃº
-          chain.doFilter(request, response);
-        }
-      }
-    } catch (ViewExpiredException g) {
-      HttpSession session = req.getSession(false);
-      if (session != null) {
-        session.invalidate();
-      }
-      session = req.getSession(true);
-      res.sendRedirect("/ui/login.jsf;jsessionid="
-          + session.getId());
-    }
-  }
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		try {
+			MyResponseWrapper responseWrapper = new MyResponseWrapper(req, res);
+			AplicacionMB appMB = (AplicacionMB) this.filterConfig
+					.getServletContext().getAttribute("aplicacionMB");
+			String url = req.getRequestURI();
+			if (url.indexOf("javax.faces.resource") != -1
+					|| url.endsWith("login.jsf")
+					|| url.endsWith("recordarContrasena.jsf")
+					|| url.endsWith("cambiarContrasena.jsf")
+					|| (appMB != null && appMB.getDebug())
+					|| url.endsWith("generadorReportes")
+					|| url.endsWith("listaReportes.jsp")
+					|| url.endsWith("compilarReporte.jsp")) {
 
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
-    this.filterConfig = filterConfig;
-  }
+				chain.doFilter(request, responseWrapper);
+			} else {
+				LOGGER.debug(url);
+				HttpSession session = req.getSession(false);
+				if (session == null
+						|| session.getAttribute("menuMB") == null
+						|| ((MenuMB) session.getAttribute("menuMB"))
+								.getOpciones() == null) {
+					if (session != null) {
+						session.removeAttribute("menuMB");
+						session.invalidate();
+					}
+					session = req.getSession(true);
+					request.setAttribute(
+							"exception",
+							new Exception(AplicacionMB.getMessage(
+									"sesionCaducado", AplicacionMB.SPANISH)));
+					this.filterConfig
+							.getServletContext()
+							.getRequestDispatcher(
+									"/ui/login.jsf;jsessionid="
+											+ session.getId())
+							.forward(request, response);
+				} else {
+					// TODO : Validar permiso a la opción de menú
+					chain.doFilter(request, responseWrapper);
+				}
+			}
+		} catch (ViewExpiredException g) {
+			HttpSession session = req.getSession(false);
+			if (session != null) {
+				session.invalidate();
+			}
+			session = req.getSession(true);
+			res.sendRedirect("/ui/login.jsf;jsessionid=" + session.getId());
+		}
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.filterConfig = filterConfig;
+	}
 
 }
