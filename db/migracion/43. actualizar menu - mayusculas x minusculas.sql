2680696eeb3b08da6adbdb1f46beddd0bf31a3f4
d5a9p6s7 <d5a9p6s7@athan>
2015-06-02 15:07:00 -0500

    Incidencias * #33 - Menu-cambiar mayusculas por minuscula

---
+++ db/migracion/43. actualizar menu - mayusculas x minusculas.sql
@@ -0,0 +1,4 @@
+﻿-- para habilitar una opción del menu
+SELECT * FROM funcionalidades ORDER BY id DESC;
+UPDATE funcionalidades SET descripcion = 'Comercio Exterior Reportes', nombre = 'Comercio Exterior Reportes' WHERE id = 110;
+UPDATE funcionalidades SET descripcion = 'Administración del sistema', nombre = 'Seguridad' WHERE id = 1;
