b9875b1d64506a5e8c90fe21018a9148a85078a5
d5a9p6s7 <dpoveda@gmail.com>
2015-08-04 16:58:13 -0500

    comercio exterior reportes - re-imprimir factura exportacion - ordenado por el consecutivo del lote oic * solucionada en desarrollo * requiere despliegue

--- app/src/main/java/com/ssl/jv/gip/web/mb/reportesComercioExterior/ReImprimirFacturaExpoMB.java
+++ app/src/main/java/com/ssl/jv/gip/web/mb/reportesComercioExterior/ReImprimirFacturaExpoMB.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
