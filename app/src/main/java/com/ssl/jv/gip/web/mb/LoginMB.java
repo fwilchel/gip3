ced554067aa39c9d25078b03b82eb153c034fd65
d5a9p6s7 <dpoveda@gmail.com>
2015-08-11 17:05:57 -0500

    seguridad - login - validaciones * solucionado en desarrollo * requiere despliegue

--- app/src/main/java/com/ssl/jv/gip/web/mb/LoginMB.java
+++ app/src/main/java/com/ssl/jv/gip/web/mb/LoginMB.java
@@ -41,145 +41,125 @@ public class LoginMB extends UtilMB {
 
   @PostConstruct
   public void init() {
-    this.empresa = 1;
-
-    System.out.println("URI " + this.getRequestURI());
-    if (this.getRequestURI().contains("loginCN")) {
-      this.menu.setTema("cn");
-    } else {
-      this.menu.setTema("default");
-    }
-
+	this.empresa = 1;
+	System.out.println("URI " + this.getRequestURI());
+	if (this.getRequestURI().contains("loginCN")) {
+	  this.menu.setTema("cn");
+	} else {
+	  this.menu.setTema("default");
+	}
   }
 
   public String ingresar() {
-	  if (this.getRequestURI().contains("loginCN")) {
-	      this.menu.setTema("cn");
-	    } else {
-	      this.menu.setTema("default");
-	    }
-    int NUMERO_MAX_LOGIN = 3;
-
-    String remoteAddr = this.getRemoteAddress();
-
-    LOGGER.info("|Client IP address=|" + remoteAddr + " |Identificacion=|" + email);
-
-    usuario = this.admonEjb.findUsuarioByEmail(this.email);
-    if (usuario != null) {
-      String pwd = Utilidad.cifrar(this.password);
-      if (usuario.getContrasena().equals(pwd)) {
-
-        if (usuario.getRole() == null) {
-          this.addMensajeError(AplicacionMB.getMessage("usuarioSinRol", language));
-          return null;
-        }
-        if (usuario.getActivo() == false) {
-          this.addMensajeError(AplicacionMB.getMessage("usuarioInactivo", language));
-          return null;
-        }
-        if (usuario.getIntentos() >= NUMERO_MAX_LOGIN) {
-          this.addMensajeError(AplicacionMB.getMessage("cuentaBloqueada", language));
-          return null;
-        }
-
-        usuario.setIntentos(0L);
-        this.admonEjb.actualizarUsuario(usuario);
-
-        List<HistorialContrasena> h = this.admonEjb.consultarHistorialContrasena(usuario);
-        if (h.size() > 0) {
-          menu.setMenu(admonEjb.getMenu(usuario.getEmail()));
-          menu.setUsuario(usuario);
-          menu.setLanguage(language);
-          if (menu.getTema().equals("cn")) {
-            try {
-              menu.setPuntoVenta(comunEJB.consultarPuntoVentaPorUsuario(usuario.getId()));
-            } catch (Exception ex) {
-              LOGGER.error("el usuario no se ha asociado a un punto de venta");
-            }
-          }
-//          String tema = getRequestParameter("tema");
-//          if (tema == null) {
-//            menu.setTema("default");
-//          } else {
-//            switch (tema) {
-//              case "cn":
-//                menu.setTema("cn");
-//                break;
-//              default:
-//                menu.setTema("default");
-//                break;
-//            }
-//          }
-          return "introduccion";
-        } else {
-          this.addMensajeError(AplicacionMB.getMessage("loginHistorialContrasena", language));
-          return "cambiarContrasena";
-        }
-
-      } else {
-        usuario.setIntentos(usuario.getIntentos() + 1);
-        this.admonEjb.actualizarUsuario(usuario);
-        this.addMensajeError(AplicacionMB.getMessage("vuelvaIntentarlo", language));
-        return null;
-      }
-    } else {
-      LOGGER.warn("|Client IP address=|" + remoteAddr + " |Identificacion=|" + email + " " + AplicacionMB.getMessage("vuelvaIntentarlo", language));
-      this.addMensajeError(AplicacionMB.getMessage("vuelvaIntentarlo", language));
-      return null;
-    }
+	if (this.getRequestURI().contains("loginCN")) {
+	  this.menu.setTema("cn");
+	} else {
+	  this.menu.setTema("default");
+	}
+	int NUMERO_MAX_LOGIN = 3;
+	String remoteAddr = this.getRemoteAddress();
+	LOGGER.info("|Client IP address=|" + remoteAddr + " |Identificacion=|" + email);
+	usuario = this.admonEjb.findUsuarioByEmail(this.email);
+	if (usuario != null) {
+	  if (usuario.getActivo() == false) {
+		this.addMensajeError(AplicacionMB.getMessage("usuarioInactivo", language));
+		return null;
+	  } else if (usuario.getIntentos() >= NUMERO_MAX_LOGIN) {
+		this.addMensajeError(AplicacionMB.getMessage("cuentaBloqueada", language));
+		return null;
+	  } else if (usuario.getRole() == null) {
+		this.addMensajeError(AplicacionMB.getMessage("usuarioSinRol", language));
+		return null;
+	  } else {
+		String pwd = Utilidad.cifrar(this.password);
+		if (usuario.getContrasena().equals(pwd)) {
+		  usuario.setIntentos(0L);
+		  this.admonEjb.actualizarUsuario(usuario);
+		  List<HistorialContrasena> h = this.admonEjb.consultarHistorialContrasena(usuario);
+		  if (h.size() > 0) {
+			menu.setMenu(admonEjb.getMenu(usuario.getEmail()));
+			menu.setUsuario(usuario);
+			menu.setLanguage(language);
+			if (menu.getTema().equals("cn")) {
+			  try {
+				menu.setPuntoVenta(comunEJB.consultarPuntoVentaPorUsuario(usuario.getId()));
+			  } catch (Exception ex) {
+				LOGGER.error("el usuario no se ha asociado a un punto de venta");
+			  }
+			}
+			return "introduccion";
+		  } else {
+			this.addMensajeError(AplicacionMB.getMessage("loginHistorialContrasena", language));
+			return "cambiarContrasena";
+		  }
+		} else {
+		  usuario.setIntentos(usuario.getIntentos() + 1);
+		  if (usuario.getIntentos() >= NUMERO_MAX_LOGIN){
+			usuario.setActivo(false);
+		  }
+		  this.admonEjb.actualizarUsuario(usuario);
+		  this.addMensajeError(AplicacionMB.getMessage("vuelvaIntentarlo", language));
+		  return null;
+		}
+	  }
+	} else {
+	  LOGGER.warn("|Client IP address=|" + remoteAddr + " |Identificacion=|" + email + " " + AplicacionMB.getMessage("vuelvaIntentarlo", language));
+	  this.addMensajeError(AplicacionMB.getMessage("vuelvaIntentarlo", language));
+	  return null;
+	}
   }
 
   public String recordarContrasena() {
-    Usuario usuario = null;
-    usuario = this.admonEjb.findUsuarioByEmail(this.email);
-    if (usuario != null) {
-      this.admonEjb.enviarEmail(usuario.getEmail(), Utilidad.descifrar(usuario.getContrasena()), usuario.getNombre(), usuario.getApellidos());
-      return "login";
+	Usuario usuario = null;
+	usuario = this.admonEjb.findUsuarioByEmail(this.email);
+	if (usuario != null) {
+	  this.admonEjb.enviarEmail(usuario.getEmail(), Utilidad.descifrar(usuario.getContrasena()), usuario.getNombre(), usuario.getApellidos());
+	  return "login";
 
-    } else {
-      this.addMensajeError(AplicacionMB.getMessage("recordarMensajeError", language));
-      return null;
-    }
+	} else {
+	  this.addMensajeError(AplicacionMB.getMessage("recordarMensajeError", language));
+	  return null;
+	}
   }
 
   public MenuMB getMenu() {
-    return menu;
+	return menu;
   }
 
   public void setMenu(MenuMB menu) {
-    this.menu = menu;
+	this.menu = menu;
   }
 
   public String getEmail() {
-    return email;
+	return email;
   }
 
   public void setEmail(String email) {
-    this.email = email;
+	this.email = email;
   }
 
   public String getPassword() {
-    return password;
+	return password;
   }
 
   public Usuario getUsuario() {
-    return usuario;
+	return usuario;
   }
 
   public void setUsuario(Usuario usuario) {
-    this.usuario = usuario;
+	this.usuario = usuario;
   }
 
   public void setPassword(String password) {
-    this.password = password;
+	this.password = password;
   }
 
   public Integer getEmpresa() {
-    return empresa;
+	return empresa;
   }
 
   public void setEmpresa(Integer empresa) {
-    this.empresa = empresa;
+	this.empresa = empresa;
   }
 
 }
