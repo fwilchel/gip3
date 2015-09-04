f170a90f2244cb9ab2e92866d57318b6dbebb631
jherediab <jherediab@gmail.com>
2015-09-04 17:43:34 -0500

    script actualizar secuencias

--- db/migracion/52. actualizar secuencias.sql
+++ db/migracion/52. actualizar secuencias.sql
@@ -4,3 +4,4 @@ SELECT setval('productosxcliente_id_seq1', MAX(id)) FROM productosxcliente;
 
 SELECT setval('terminos_transporte_id_seq', MAX(id)) FROM terminos_transporte;
 
+SELECT setval('movimientos_inventario_comext_id_seq', MAX(id)) FROM movimientos_inventario_comext;
