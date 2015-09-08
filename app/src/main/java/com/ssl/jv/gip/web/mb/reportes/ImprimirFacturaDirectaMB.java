04f51d0c0993a8cb8f75db9758d83ee7d61e8e9c
jherediab <jherediab@gmail.com>
2015-09-08 15:41:49 -0500

    ventas y facturacion - imprimir fd

--- app/src/main/java/com/ssl/jv/gip/web/mb/reportes/ImprimirFacturaDirectaMB.java
+++ app/src/main/java/com/ssl/jv/gip/web/mb/reportes/ImprimirFacturaDirectaMB.java
@@ -22,7 +22,6 @@ import com.ssl.jv.gip.jpa.pojo.Documento;
 import com.ssl.jv.gip.negocio.dto.FacturaDirectaDTO;
 import com.ssl.jv.gip.negocio.dto.ProductoFacturaDirectaDTO;
 import com.ssl.jv.gip.negocio.dto.ProductoLoteAsignarLoteOICDTO;
-import com.ssl.jv.gip.negocio.ejb.ComercioExteriorEJB;
 import com.ssl.jv.gip.web.mb.UtilMB;
 import com.ssl.jv.gip.web.mb.util.ConstantesDocumento;
 import com.ssl.jv.gip.web.mb.util.ConstantesTipoDocumento;
@@ -57,225 +56,142 @@ import com.ssl.jv.gip.negocio.ejb.VentasFacturacionEJB;
 @SessionScoped
 public class ImprimirFacturaDirectaMB extends UtilMB {
 
-	private List<Documento> list;
-	private StreamedContent reportePDF;
+  @EJB
+  private VentasFacturacionEJB ventasFacturacionEjb;
+  private List<Documento> list;
+  private StreamedContent reportePDF;
+  private String consecutivoDocumento;
+  private Documento fd;
+  private FacturaDirectaDTO fdDTO;
+  private List<ProductoFacturaDirectaDTO> listaDetalle;
+  private List<ProductoLoteAsignarLoteOICDTO> lotes;
 
-	public List<Documento> getList() {
-		return list;
-	}
+  public List<Documento> consultarDocumento() {
+    Map<String, Object> parametros = new HashMap<>();
+    String parametroConseDoc;
+    if (this.consecutivoDocumento.equals("")) {
+      parametroConseDoc = "%";
+    } else {
+      parametroConseDoc = "%" + this.consecutivoDocumento + "%";
+    }
+    parametros.put("tipo", ConstantesTipoDocumento.FACTURA);
+    Long[] array = new Long[2];
+    array[0] = (long) ConstantesDocumento.IMPRESO;
+    array[1] = (long) ConstantesDocumento.ANULADO;
+    parametros.put("estado", array);
+    parametros.put("parametroConseDoc", parametroConseDoc);
+    list = ventasFacturacionEjb.consultarDocumento(parametros, array);
+    return list;
+  }
 
-	public void setList(List<Documento> list) {
-		this.list = list;
-	}
+  public void setFd(Documento fd) {
+    this.fd = fd;
+    this.setFdDTO(this.ventasFacturacionEjb.consultarDocumentoFacturaDirecta(fd.getConsecutivoDocumento()));
+    listaDetalle = this.ventasFacturacionEjb.consultarProductoFacturaDirecta(fd.getConsecutivoDocumento());
+    System.out.println("doc: " + fd.getId());
+    System.out.println("cliente: " + fd.getCliente().getId());
+  }
 
-	private String strConsecutivoDocumento;
-	private Documento seleccionado;
-	private FacturaDirectaDTO seleccionado2;
+  public StreamedContent getReportePDF() {
+    Map<String, Object> parametros = new HashMap<>();
+    int n = 0;
+    SimpleDateFormat ft = new SimpleDateFormat("dd-MM-yyyy");
+    String fechaStringGeneracion = ft.format(fdDTO.getFechaGeneracion());
+    parametros.put("cliente", fdDTO.getNombreCliente());
+    parametros.put("nit", fdDTO.getNitCliente());
+    parametros.put("ciudad", fdDTO.getNombreCiudadCliente());
+    parametros.put("direccion", fdDTO.getDireccionCliente());
+    parametros.put("telefono", fdDTO.getTelefonoCliente());
+    parametros.put("numFactura", fdDTO.getConsecutivoDocumento());
+    parametros.put("despachado_a", fdDTO.getNombrePuntoVenta());
+    parametros.put("direccionpv", fdDTO.getDireccionPuntoVenta());
+    parametros.put("telefonopv", "Teléfono: " + fdDTO.getTelefonoPuntoVenta());
+    parametros.put("ciudadpv", fdDTO.getNombreCiudadPuntoVenta());
+    parametros.put("documento", fd.getDocumentoCliente());
+    parametros.put("fecha", fechaStringGeneracion);
+    parametros.put("tipoImp", "Copia");
+    //parametros.put("tipoImp", "Original");
+    parametros.put("valorSubtotal", fdDTO.getValorSubtotal());
+    parametros.put("valorDescuento", fdDTO.getValorDescuento());
+    parametros.put("valorIva5", fdDTO.getValorIva5());
+    parametros.put("valorIva16", fdDTO.getValorIva16());
+    parametros.put("valorTotal", fdDTO.getValorTotal());
+    Numero_a_Letra NumLetra = new Numero_a_Letra();
+    parametros.put("valorLetras", NumLetra.Convertir(fdDTO.getValorTotal().toString(), true));
+    if (fdDTO.getEstado() == ConstantesDocumento.ANULADO) {
+      parametros.put("anulada", "ANULADA");
+    } else {
+      parametros.put("anulada", "");
+    }
+    for (ProductoFacturaDirectaDTO p : listaDetalle) {
+      String mark = p.getMarca();
+      if (mark.equals("(*)")) {
+        n = n + 1;
+      }
+    }
+    if (n > 0) { // Existe algún registro con descuento adicioanl
+      parametros.put("descuentoCliente", fdDTO.getDescuentoCliente().toString() + "%");
+      parametros.put("observaciones", "Los productos marcados con (*) incluyen descuento adicional.");
+      parametros.put("mark", "(*)");
+    } else {
+      parametros.put("descuentoCliente", "");
+      parametros.put("observaciones", "");
+      parametros.put("mark", "");
+    }
+    JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(listaDetalle);
+    try {
+      Hashtable<String, String> parametrosR = new Hashtable<>();
+      parametrosR.put("tipo", "pdf");
+      String reporte = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/reportes/report2.jasper");
+      ByteArrayOutputStream os = (ByteArrayOutputStream) com.ssl.jv.gip.util.GeneradorReportes.generar(parametrosR, reporte, null, null, null, parametros, ds);
+      reportePDF = new DefaultStreamedContent(new ByteArrayInputStream(os.toByteArray()), "application/pdf ", fdDTO.getConsecutivoDocumento() + "_copia.pdf");
+    } catch (Exception e) {
+      this.addMensajeError(e);
+    }
+    return reportePDF;
+  }
 
-	public FacturaDirectaDTO getSeleccionado2() {
-		return seleccionado2;
-	}
+  public List<Documento> getList() {
+    return list;
+  }
 
-	public void setSeleccionado2(FacturaDirectaDTO seleccionado2) {
-		this.seleccionado2 = seleccionado2;
-	}
+  public void setList(List<Documento> list) {
+    this.list = list;
+  }
 
-	private List<ProductoFacturaDirectaDTO> listaDetalle;
+  public FacturaDirectaDTO getFdDTO() {
+    return fdDTO;
+  }
 
-	private List<ProductoLoteAsignarLoteOICDTO> lotes;
+  public void setFdDTO(FacturaDirectaDTO fdDTO) {
+    this.fdDTO = fdDTO;
+  }
 
-	public List<ProductoLoteAsignarLoteOICDTO> getLotes() {
-		return lotes;
-	}
+  public List<ProductoLoteAsignarLoteOICDTO> getLotes() {
+    return lotes;
+  }
 
-	public void setLotes(List<ProductoLoteAsignarLoteOICDTO> lotes) {
-		this.lotes = lotes;
-	}
+  public void setLotes(List<ProductoLoteAsignarLoteOICDTO> lotes) {
+    this.lotes = lotes;
+  }
 
-	public List<ProductoFacturaDirectaDTO> getListaDetalle() {
-		return listaDetalle;
-	}
+  public List<ProductoFacturaDirectaDTO> getListaDetalle() {
+    return listaDetalle;
+  }
 
-	public void setListaDetalle(List<ProductoFacturaDirectaDTO> listaDetalle) {
-		this.listaDetalle = listaDetalle;
-	}
+  public void setListaDetalle(List<ProductoFacturaDirectaDTO> listaDetalle) {
+    this.listaDetalle = listaDetalle;
+  }
 
-	public Documento getSeleccionado() {
-		return seleccionado;
-	}
+  public Documento getFd() {
+    return fd;
+  }
 
-	public void setSeleccionado(Documento seleccionado) {
+  public String getConsecutivoDocumento() {
+    return consecutivoDocumento;
+  }
 
-		this.seleccionado = seleccionado;
-
-		this.setSeleccionado2(this.ventasFacturacionEjb.consultarDocumentoFacturaDirecta(seleccionado.getConsecutivoDocumento()));
-
-		listaDetalle = this.ventasFacturacionEjb.consultarProductoFacturaDirecta(seleccionado.getConsecutivoDocumento());
-
-		System.out.println("doc: " + seleccionado.getId());
-		System.out.println("cliente: " + seleccionado.getCliente().getId());
-
-		// lotes =
-		// comercioEjb.consultarProductoPorDocumentoLoteAsignarLotesOIC(seleccionado.getId(),
-		// seleccionado.getCliente().getId());
-	}
-
-	public String getStrConsecutivoDocumento() {
-		return strConsecutivoDocumento;
-	}
-
-	public void setStrConsecutivoDocumento(String strConsecutivoDocumento) {
-		this.strConsecutivoDocumento = strConsecutivoDocumento;
-	}
-
-	@EJB
-	private VentasFacturacionEJB ventasFacturacionEjb;
-
-	@EJB
-	private ComercioExteriorEJB comercioEjb;
-
-	public List<Documento> consultarDocumento() {
-		Map<String, Object> parametros = new HashMap<>();
-
-		// Se Define la cadena de busqueda del nombre segun lo ingresado en el
-		// campo de Consecutivo Pedido.
-		String parametroConseDoc;
-		if (this.strConsecutivoDocumento.equals("")) {
-			parametroConseDoc = "%";
-		} else {
-			parametroConseDoc = "%" + this.strConsecutivoDocumento + "%";
-		}
-
-		parametros.put("tipo", ConstantesTipoDocumento.FACTURA);
-		// parametros.put("estado", ConstantesDocumento.IMPRESO);
-		// parametros.put("estado2", ConstantesDocumento.ANULADO);
-
-		Long[] array = new Long[2];
-		array[0] = (long) ConstantesDocumento.IMPRESO;
-		array[1] = (long) ConstantesDocumento.ANULADO;
-		parametros.put("estado", array);
-
-		parametros.put("parametroConseDoc", parametroConseDoc);
-
-		list = ventasFacturacionEjb.consultarDocumento(parametros, array);
-
-		return list;
-	}
-
-	public StreamedContent getReportePDF() {
-		Map<String, Object> parametros = new HashMap<>();
-		// Cliente c=seleccionado.getCliente();
-
-		int n = 0;
-
-		/*
-		 * parametros.put("despachado_a",pv.getStrNombre());
-		 * parametros.put("direccionpv",pv.getStrDireccion());
-		 * parametros.put("telefonopv","Teléfono: " + pv.getStrTelefono());
-		 * parametros.put("ciudadpv",pv.getStrNomCiudad());
-		 * parametros.put("documento",docCabecera.getStrDocumentoCliente());
-		 * parametros.put("fecha",fechaStringGeneracion);
-		 * parametros.put("sku","A28");
-		 */
-		// parametros.put("numFactura", docCabecera.getStrConsecutivoDocumento());
-		// parametros.put("tipoImp", "Copia");
-		SimpleDateFormat ft = new SimpleDateFormat("dd-MM-yyyy");
-		String fechaStringGeneracion = ft.format(seleccionado2.getFechaGeneracion());
-
-		parametros.put("cliente", seleccionado2.getNombreCliente());
-		parametros.put("nit", seleccionado2.getNitCliente());
-		parametros.put("ciudad", seleccionado2.getNombreCiudadCliente());
-		parametros.put("direccion", seleccionado2.getDireccionCliente());
-		parametros.put("telefono", seleccionado2.getTelefonoCliente());
-		// parametros.put("contacto", c.getContacto());
-		// parametros.put("id_documento", seleccionado.getConsecutivoDocumento());
-		// parametros.put("solicitud", seleccionado.getObservacionDocumento());
-		parametros.put("numFactura", seleccionado2.getConsecutivoDocumento());
-
-		/*
-		 * System.out.println("cliente: "+c.getNombre());
-		 * System.out.println("NIT: "+c.getNit());
-		 * System.out.println("ciudad: "+c.getCiudad().getNombre());
-		 * System.out.println("numFactura"+seleccionado.getConsecutivoDocumento());
-		 * 
-		 * System.out.println("direccion"+ c.getDireccion());
-		 * System.out.println("telefono"+ c.getTelefono());
-		 * System.out.println("fecha"+fechaStringGeneracion);
-		 * System.out.println("documento"+seleccionado.getDocumentoCliente());
-		 */
-		// System.out.println("contacto"+ c.getContacto());
-		// parametros.put("id_documento", seleccionado.getConsecutivoDocumento());
-		parametros.put("despachado_a", seleccionado2.getNombrePuntoVenta());
-		parametros.put("direccionpv", seleccionado2.getDireccionPuntoVenta());
-		parametros.put("telefonopv", "Teléfono: " + seleccionado2.getTelefonoPuntoVenta());
-		parametros.put("ciudadpv", seleccionado2.getNombreCiudadPuntoVenta());
-		parametros.put("documento", seleccionado.getDocumentoCliente());
-		parametros.put("fecha", fechaStringGeneracion);
-		parametros.put("tipoImp", "Copia");
-		// parametros.put("tipoImp", "Original");
-		parametros.put("valorSubtotal", seleccionado2.getValorSubtotal());
-		parametros.put("valorDescuento", seleccionado2.getValorDescuento());
-		parametros.put("valorIva5", seleccionado2.getValorIva5());
-		parametros.put("valorIva16", seleccionado2.getValorIva16());
-		parametros.put("valorTotal", seleccionado2.getValorTotal());
-
-		Numero_a_Letra NumLetra = new Numero_a_Letra();
-		parametros.put("valorLetras", NumLetra.Convertir(seleccionado2.getValorTotal().toString(), true));
-
-		if (seleccionado2.getEstado() == ConstantesDocumento.ANULADO) {
-			parametros.put("anulada", "ANULADA");
-		} else {
-			parametros.put("anulada", "");
-		}
-
-		for (ProductoFacturaDirectaDTO p : listaDetalle) {
-
-			String mark = p.getMarca();
-			if (mark.equals("(*)")) {
-				n = n + 1;
-			}
-
-		}
-
-		if (n > 0) { // Existe algún registro con descuento adicioanl
-			parametros.put("descuentoCliente", seleccionado2.getDescuentoCliente().toString() + "%");
-			parametros.put("observaciones", "Los productos marcados con (*) incluyen descuento adicional.");
-			parametros.put("mark", "(*)");
-		} else {
-			parametros.put("descuentoCliente", "");
-			parametros.put("observaciones", "");
-			parametros.put("mark", "");
-		}
-
-		// parametros.put("mark", "(*)");
-		// parametros.put("descuentoCliente","%");
-		// parametros.put("observaciones",
-		// "Los productos marcados con (*) incluyen descuento adicional.");
-		JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(listaDetalle);
-		try {
-
-			Hashtable<String, String> parametrosR = new Hashtable<>();
-			parametrosR.put("tipo", "pdf");
-			String reporte = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/reportes/report2.jasper");
-
-			ByteArrayOutputStream os = (ByteArrayOutputStream) com.ssl.jv.gip.util.GeneradorReportes.generar(parametrosR, reporte, null, null, null, parametros, ds);
-			reportePDF = new DefaultStreamedContent(new ByteArrayInputStream(os.toByteArray()), "application/pdf ", seleccionado2.getConsecutivoDocumento() + "_copia.pdf");
-
-			/*
-			 * String xmlfile =
-			 * FacesContext.getCurrentInstance().getExternalContext().
-			 * getRealPath("/reportes/report2.jrxml"); JasperReport reporte =
-			 * JasperCompileManager.compileReport(xmlfile); JasperPrint print =
-			 * JasperFillManager.fillReport(reporte, parametros, ds);
-			 * ByteArrayOutputStream baos =new ByteArrayOutputStream();
-			 * net.sf.jasperreports
-			 * .engine.JasperExportManager.exportReportToPdfStream(print,baos);
-			 */
-		} catch (Exception e) {
-			this.addMensajeError(e);
-		}
-
-		return reportePDF;
-	}
-
+  public void setConsecutivoDocumento(String consecutivoDocumento) {
+    this.consecutivoDocumento = consecutivoDocumento;
+  }
 }
