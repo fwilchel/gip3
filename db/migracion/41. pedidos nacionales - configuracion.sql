1996da1e2cee16bd501ba085d978818c1cc34d80
jherediab <jherediab@gmail.com>
2015-09-16 10:50:37 -0500

    pedidos nacionales - configuracion * script de datos de configuracion para dar de alto esta opcion

--- db/migracion/41. pedidos nacionales - configuracion.sql
+++ db/migracion/41. pedidos nacionales - configuracion.sql
@@ -1,4 +1,4 @@
-﻿-- para habilitar una opción del menu
+-- para habilitar una opción del menu
 SELECT * FROM funcionalidades ORDER BY id DESC;
 INSERT INTO funcionalidades (id, descripcion, nombre) VALUES (144, 'Pedidos Nacionales', 'Pedidos Nacionales');
 INSERT INTO funcionalidades (id, descripcion, nombre, id_funcionalidad_padre, ruta) VALUES (145, 'Ingresar Solicitud', 'Ingresar Solicitud', 144, 'ingresar_solicitud_pedido_cn');
@@ -6,5 +6,11 @@ SELECT * FROM permisos ORDER BY id DESC;
 INSERT INTO permisos  VALUES (144,144,'T');
 INSERT INTO permisos  VALUES (145,145,'T');
 SELECT * FROM permisos_roles ORDER BY id_permiso DESC;
-INSERT INTO permisos_roles VALUES (144,1);
-INSERT INTO permisos_roles VALUES (145,1);
+INSERT INTO permisos_roles VALUES (144,1); -- role administrador 
+INSERT INTO permisos_roles VALUES (145,1); -- role administrador
+INSERT INTO roles VALUES (40, 'Pedidos Nacionales');
+INSERT INTO permisos_roles VALUES (144,40); -- role administrador 
+INSERT INTO permisos_roles VALUES (145,40); -- role administrador
+-- insertar usuario para pruebas.
+-- asignar el usuario a un punto de venta
+-- en la consulta deben aparecer los productos por cliente y punto de venta vigentes 
