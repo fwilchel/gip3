19491c16bebb46094149d8855b8fcbf24966fba1
d5a9p6s7 <dpoveda@gmail.com>
2015-09-28 05:34:28 -0500

    comercio exterior - anular SP * en desarrollo

--- app/src/main/java/com/ssl/jv/gip/web/mb/comercioexterior/AnularSPMB.java
+++ app/src/main/java/com/ssl/jv/gip/web/mb/comercioexterior/AnularSPMB.java
@@ -11,26 +11,34 @@ import org.apache.log4j.Logger;
 
 import com.ssl.jv.gip.jpa.pojo.Documento;
 import com.ssl.jv.gip.jpa.pojo.LogAuditoria;
+import com.ssl.jv.gip.negocio.dto.FiltroDocumentoDTO;
 import com.ssl.jv.gip.negocio.ejb.ComercioExteriorEJBLocal;
+import com.ssl.jv.gip.negocio.ejb.ComunEJBLocal;
 import com.ssl.jv.gip.web.mb.AplicacionMB;
 import com.ssl.jv.gip.web.mb.MenuMB;
 import com.ssl.jv.gip.web.mb.UtilMB;
+import com.ssl.jv.gip.web.mb.util.ConstantesDocumento;
+import com.ssl.jv.gip.web.mb.util.ConstantesTipoDocumento;
 import com.ssl.jv.gip.web.util.Utilidad;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 /**
  * <p>
- * Title: GIP</p>
+ * Title: GIP
+ * </p>
  *
  * <p>
- * Description: GIP</p>
+ * Description: GIP
+ * </p>
  *
  * <p>
- * Copyright: Copyright (c) 2014</p>
+ * Copyright: Copyright (c) 2014
+ * </p>
  *
  * <p>
- * Company: Soft Studio Ltda.</p>
+ * Company: Soft Studio Ltda.
+ * </p>
  *
  * @author Diego Poveda
  * @email dpoveda@gmail.com
@@ -41,47 +49,33 @@ import javax.inject.Named;
 @ViewScoped
 public class AnularSPMB extends UtilMB {
 
-  /**
-   *
-   */
   private static final long serialVersionUID = -2780795923623719268L;
   private static final Logger LOGGER = Logger.getLogger(AnularSPMB.class);
   private final Integer language = AplicacionMB.SPANISH;
   @EJB
   private ComercioExteriorEJBLocal comercioExteriorEjb;
+  @EJB
+  private ComunEJBLocal comunEJB;
   @ManagedProperty(value = "#{aplicacionMB}")
   private AplicacionMB appMB;
   @ManagedProperty(value = "#{menuMB}")
   private MenuMB menu;
-  /**
-   * Consecutivo por el cual se va a realizar la busqueda
-   */
-  private String consecutivoDocumento;
-  /**
-   * lista de documentos que retorna la consulta a la base de datos
-   */
-  private List<Documento> listaDocumentos;
-  /**
-   * Registro seleccionado
-   */
-  private Documento seleccionado;
-
-  public AnularSPMB() {
-  }
+  private FiltroDocumentoDTO filtro;
+  private List<Documento> listaSP;
+  private Documento sp;
 
   @PostConstruct
   public void init() {
-    LOGGER.debug("Metodo: <<init>>");
+	LOGGER.debug("Metodo: <<init>>");
+	this.filtro = new FiltroDocumentoDTO();
   }
 
-  /**
-   *
-   */
-  public void buscar() {
-    LOGGER.debug("Metodo: <<buscar>>");
-    // traer documentos q no esten en estado ANULADO, CERRADO.
-    // y q no se haya generado la FP
-    setListaDocumentos(comercioExteriorEjb.consultarSolicitudesPedidoPorAnular(getConsecutivoDocumento()));
+  public void consultarListaSP() {
+	filtro.setTipoDocumento(new Long(ConstantesTipoDocumento.SOLICITUD_PEDIDO));
+	filtro.setEstado((long) ConstantesDocumento.ACTIVO, (long) ConstantesDocumento.VERIFICADO);
+	filtro.setOrdenCampo("consecutivoDocumento DESC");
+	this.listaSP = comunEJB.consultarDocumentos(filtro);
+	//setListaSP(comercioExteriorEjb.consultarSolicitudesPedidoPorAnular(getConsecutivoDocumento()));
   }
 
   /**
@@ -89,39 +83,38 @@ public class AnularSPMB extends UtilMB {
    * @param documento
    */
   public void seleccionarSolicitudPedido(Documento documento) {
-    LOGGER.debug("Metodo: <<seleccionarSolicitudPedido>>");
-    seleccionado = documento;
+	LOGGER.debug("Metodo: <<seleccionarSolicitudPedido>>");
+	sp = documento;
   }
 
   /**
    *
    */
   public void anularSolicitudPedido() {
-    LOGGER.debug("Metodo: <<anularSolicitudPedido>>");
-    try {
-      LogAuditoria auditoria = new LogAuditoria();
-      auditoria.setIdUsuario(menu.getUsuario().getId());
-      auditoria.setIdFuncionalidad(menu.getIdOpcionActual());
-      comercioExteriorEjb.anularDocumentoComercioExterior(seleccionado, auditoria);
-      listaDocumentos.remove(seleccionado);
-      this.addMensajeInfo("listMsgs", AplicacionMB.getMessage("spaMsgExito", language));
-    } catch (EJBTransactionRolledbackException e) {
-      // TODO: validar que excepciones se denen tener en cuenta.
-      this.addMensajeError(AplicacionMB.getMessage("spaMsgError", language));
-      LOGGER.error(e);
-    } catch (Exception e) {
-      this.addMensajeError(AplicacionMB.getMessage("spaMsgError", language));
-      LOGGER.error(e);
-    }
+	LOGGER.debug("Metodo: <<anularSolicitudPedido>>");
+	try {
+	  LogAuditoria auditoria = new LogAuditoria();
+	  auditoria.setIdUsuario(menu.getUsuario().getId());
+	  auditoria.setIdFuncionalidad(menu.getIdOpcionActual());
+	  comercioExteriorEjb.anularDocumentoComercioExterior(sp, auditoria);
+	  listaSP.remove(sp);
+	  this.addMensajeInfo("listMsgs", AplicacionMB.getMessage("spaMsgExito", language));
+	} catch (EJBTransactionRolledbackException e) {
+	  // TODO: validar que excepciones se denen tener en cuenta.
+	  this.addMensajeError(AplicacionMB.getMessage("spaMsgError", language));
+	  LOGGER.error(e);
+	} catch (Exception e) {
+	  this.addMensajeError(AplicacionMB.getMessage("spaMsgError", language));
+	  LOGGER.error(e);
+	}
   }
 
   /**
    *
    */
   public void cancelar() {
-    LOGGER.debug("Metodo: <<cancelar>>");
-    this.seleccionado = null;
-    this.consecutivoDocumento = "";
+	LOGGER.debug("Metodo: <<cancelar>>");
+	this.sp = null;
   }
 
   /**
@@ -129,47 +122,47 @@ public class AnularSPMB extends UtilMB {
    * @return
    */
   public String getMensajeAdvertencia() {
-    LOGGER.debug("Metodo: <<getMensajeAdvertencia>>");
-    if (seleccionado == null) {
-      return null;
-    } else {
-      String mensajeAdvertencia = AplicacionMB.getMessage("spaMsgAdvertencia", language);
-      String parametros[] = new String[1];
-      parametros[0] = seleccionado.getConsecutivoDocumento();
-      mensajeAdvertencia = Utilidad.stringFormat(mensajeAdvertencia, parametros);
-      return mensajeAdvertencia;
-    }
+	LOGGER.debug("Metodo: <<getMensajeAdvertencia>>");
+	if (sp == null) {
+	  return null;
+	} else {
+	  String mensajeAdvertencia = AplicacionMB.getMessage("spaMsgAdvertencia", language);
+	  String parametros[] = new String[1];
+	  parametros[0] = sp.getConsecutivoDocumento();
+	  mensajeAdvertencia = Utilidad.stringFormat(mensajeAdvertencia, parametros);
+	  return mensajeAdvertencia;
+	}
   }
 
   public void setAppMB(AplicacionMB appMB) {
-    this.appMB = appMB;
+	this.appMB = appMB;
   }
 
   public void setMenu(MenuMB menu) {
-    this.menu = menu;
+	this.menu = menu;
   }
 
-  public String getConsecutivoDocumento() {
-    return consecutivoDocumento;
+  public FiltroDocumentoDTO getFiltro() {
+	return filtro;
   }
 
-  public void setConsecutivoDocumento(String consecutivoDocumento) {
-    this.consecutivoDocumento = consecutivoDocumento;
+  public void setFiltro(FiltroDocumentoDTO filtro) {
+	this.filtro = filtro;
   }
 
-  public List<Documento> getListaDocumentos() {
-    return listaDocumentos;
+  public List<Documento> getListaSP() {
+	return listaSP;
   }
 
-  public void setListaDocumentos(List<Documento> listaDocumentos) {
-    this.listaDocumentos = listaDocumentos;
+  public void setListaSP(List<Documento> listaSP) {
+	this.listaSP = listaSP;
   }
 
-  public Documento getSeleccionado() {
-    return seleccionado;
+  public Documento getSp() {
+	return sp;
   }
 
-  public void setSeleccionado(Documento seleccionado) {
-    this.seleccionado = seleccionado;
+  public void setSp(Documento sp) {
+	this.sp = sp;
   }
 }
